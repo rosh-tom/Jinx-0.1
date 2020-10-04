93d9c2b691f04167f4a6ddda5f48e07f734004b7
rosh-tom <stormking010@gmail.com>
2020-10-04 23:47:09 +0800

    First version of Jinx

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php include('inc/header.php'); ?> 
+<title>Title</title>
+    </head> 
+<body> 
+ <!-- CODE HERE   -->
+    <div class="container">
+        <?php include('inc/nav.php') ?>  <!-- Navigation -->
+
+    <!-- CODE HERE....  -->
+     
+
+    </div>  
+    <!-- .container  -->
+
+
+<!-- ============================ FOOTER  -->
+<?php include('inc/footer.php'); ?>
